11d610f605f38d3664cdf744f92217aceeb221eb
ChandreshPC <vithanichandresh.g@gmail.com>
2023-09-01 15:45:31 +0530

    set firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:testing_ci_cd/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
