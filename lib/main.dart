8fc1a782dc277244aeecec07cc095dfc7b40c72e
ChandreshPC <vithanichandresh.g@gmail.com>
2023-09-01 15:47:51 +0530

    set firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:testing_ci_cd/firebase_options.dart';
+
+import 'firebase_options.dart';
 
 Future<void> main() async {
   await Firebase.initializeApp(
